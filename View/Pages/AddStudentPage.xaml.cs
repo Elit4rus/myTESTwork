c8157a36b652e7a019efad505eab865cf33652ae
Ivan Krysenko <assuss2120@gmail.com>
2025-03-11 13:39:54 +0300

    Страница с добавлением студента и группы

---
+++ View/Pages/AddStudentPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TESTwork.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddStudentPage.xaml
+    /// </summary>
+    public partial class AddStudentPage : Page
+    {
+        public AddStudentPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
